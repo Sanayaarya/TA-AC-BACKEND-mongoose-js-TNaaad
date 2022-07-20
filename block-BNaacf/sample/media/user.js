6ab16036e1a34aad7bcca0af67efc9e97f6b0c7e
Sanaya Arya <sanayaarya0123@gmail.com>
2022-07-20 17:58:28 +0530

    Explain with code

--- block-BNaacf/sample/media/user.js
+++ block-BNaacf/sample/media/user.js
@@ -1,20 +1,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-  name:String,
-  age:Number,
-  sports:[string],
-  marks:[Number],
-  family: {
-    father:String,
-    mother:String
-  }
-},{timestamps:true})
-
-var articleschema = new Schema({
-  title:String,
-  decription:String,
-  author:Schema.Types.objectId
+var addressSchema = new Schema({
+  village:String,
+  city:String,
+  state:String,
+  pincode:Number,
+  user: Schema.Types.objectId
 })
- 
