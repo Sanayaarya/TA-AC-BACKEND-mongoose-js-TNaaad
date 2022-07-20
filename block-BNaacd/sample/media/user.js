d5ad31e54b967488b9269bba53d7fa7a382273e0
Sanaya Arya <sanayaarya0123@gmail.com>
2022-07-20 17:09:56 +0530

    Explain with code

---
+++ block-BNaacd/sample/media/user.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name: String,
+  age: Number,
+  isAdmin: Boolean,
+  createAt:Date
+},{timestamps:true})
+
+var userSchema = new Schema({
+  name:{type:String,lowercase:true}
+})
