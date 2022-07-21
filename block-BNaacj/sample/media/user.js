3e738f3ca7de6c0ac70800ddf4468f2c2fa9e76b
Sanaya Arya <sanayaarya0123@gmail.com>
2022-07-21 12:07:24 +0530

    Explain with code

--- block-BNaacj/sample/media/user.js
+++ block-BNaacj/sample/media/user.js
@@ -2,8 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  name:{type:String,required: true},
-  age:{type:Number, min:18, max:30},
-  email:{type:String, lowercase:true,trim:true,match:/@/},
-  password:{type:String,minlength:5,maxlength:20}
-},{timestamps:true})
+  name:String,
+  age:{type:Number, default:0},
+  email:{type:String, lowercase:true},
+  favourates:[String],
+  password:{type:String,minlength:5,maxlength:20},
+  createdAt:{type:Date, default:new Date()}
+})
