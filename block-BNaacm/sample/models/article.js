8129176c5ed0912e132b0213ecb8dc82c86bdcfc
Sanaya Arya <sanayaarya0123@gmail.com>
2022-07-21 13:13:53 +0530

    Explain with

--- block-BNaacm/sample/models/article.js
+++ block-BNaacm/sample/models/article.js
@@ -2,11 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  name:String,
-  age:{type:Number, default:0},
-  email:{type:String, lowercase:true},
-  favourates:[String],
-  password:{type:String,minlength:5,maxlength:20},
-  createdAt:{type:Date, default:new Date()}
+  title:String,
+  description:String
 })
 module.exports = mongoose.model('Article' , articleSchema);
